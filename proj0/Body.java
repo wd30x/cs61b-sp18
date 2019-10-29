49f3bcfd919bdc7feb3fd4900754ead4a6334880
372338552 <372338552@qq.com>
2019-10-29 20:07:22 +1300

    a new commit

---
+++ proj0/Body.java
@@ -0,0 +1,25 @@
+/** project 0, the body class */
+public class Body{
+    public double xxPos;
+    public double yyPos;
+    public double xxVel;
+    public double yyVel;
+    public double mass;
+    public String imgFileName;
+
+    /** first constructor */
+    public Body(double xP, double yP, double xV,
+                double yV, double m, String img){
+        xxPos = xP;
+        yyPos = yP;
+        xxVel = xV;
+        yyVel = yV;
+        mass = m;
+        imgFileName = img;
+    }
+
+    /** second constructor */
+    public Body(Body b){
+        Body body = b;
+    }
+}
