455be5c4569e2c92a1fbc455002b2c4519c42f98
372338552 <372338552@qq.com>
2019-11-11 00:34:08 +1300

    finish addLast and addFirst methods

--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -1,41 +1,96 @@
 public class LinkedListDeque<T> {
+    private Node sentinel;
+    private int size;
 
+    public class Node {
+        public Node prev;
+        public T item;
+        public Node next;
+
+        public Node(Node p, T i, Node n) {
+            prev = p;
+            item = i;
+            next = n;
+        }
+    }
+
+    /**
+     * Creates an empty linked list deque.
+     */
     public LinkedListDeque() {
+        Node sentinel = new Node(null, null, null);
+        sentinel.next = sentinel;
+        sentinel.prev = sentinel;
+        size = 0;
 
     }
 
+    /**
+     * Adds an item of type T to the front of the deque.
+     * no loop or recursion
+     */
     public void addFirst(T item) {
-
+        Node node = new Node(sentinel, item, sentinel.next);
+        sentinel.next.prev = node;
+        sentinel.next = node;
     }
 
+    /**
+     * Adds an item of type T to the back of the deque.
+     * no loop or recursion
+     */
     public void addLast(T item) {
-
+        Node node = new Node(sentinel.prev, item, sentinel);
+        sentinel.prev.next = node;
+        sentinel.prev = node;
     }
 
+    /**
+     * Returns true if deque is empty, false otherwise.
+     */
     public boolean isEmpty() {
         return false;
     }
 
+    /**
+     * Returns the number of items in the deque.
+     */
     public int size() {
-        return 0;
+        return this.size;
     }
 
+    /**
+     * Prints the items in the deque from first to last, separated by a space.
+     */
     public void printDeque() {
 
     }
 
+    /**
+     * Removes and returns the item at the front of the deque. If no such item exists, returns null.
+     */
     public T removeFirst() {
         return null;
     }
 
+    /**
+     * Removes and returns the item at the back of the deque. If no such item exists, returns null.
+     */
     public T removeLast() {
         return null;
     }
 
+    /**
+     * Gets the item at the given index, where 0 is the front, 1 is the next item, and so forth. If no such item
+     * exists, returns null. Must not alter the deque!
+     */
     public T get(int index) {
         return null;
     }
 
+    /**
+     * Same as get, but uses recursion.
+     */
     public T getRecursive(int index) {
         return null;
     }
