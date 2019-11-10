79e8b3e290b09f164b9b6527b2b8d51af27b4576
372338552 <372338552@qq.com>
2019-11-10 21:56:37 +1300

    Add basic deque and node classes

---
+++ proj1a/LinkedListDeque.java
@@ -0,0 +1,42 @@
+public class LinkedListDeque<T> {
+
+    public LinkedListDeque() {
+
+    }
+
+    public void addFirst(T item) {
+
+    }
+
+    public void addLast(T item) {
+
+    }
+
+    public boolean isEmpty() {
+        return false;
+    }
+
+    public int size() {
+        return 0;
+    }
+
+    public void printDeque() {
+
+    }
+
+    public T removeFirst() {
+        return null;
+    }
+
+    public T removeLast() {
+        return null;
+    }
+
+    public T get(int index) {
+        return null;
+    }
+
+    public T getRecursive(int index) {
+        return null;
+    }
+}
